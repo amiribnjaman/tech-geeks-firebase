71a95f41cb75b79d7729c40fae61f12779181441
amiribnjaman <amiribnjaman@gmail.com>
2022-04-08 12:07:13 +0600

    Logout functionalities and navbar login logout toggle added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,9 +3,30 @@ import { NavLink } from "react-router-dom";
 import Logo from "../../Assets/Image/logo.png";
 import "./Navbar.css";
 import { useLocation } from "react-router-dom";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "../Firebase/Firebase.init";
 
 const Navbar = () => {
   const { pathname } = useLocation();
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+
+      }
+    });
+  }, [])
+
+  const handleLogout = () => {
+    signOut(auth).then(() => {
+      setUser({});
+    }).catch((error) => {
+      // An error happened.
+    });
+  }
 
   return (
     <nav
@@ -29,12 +50,12 @@ const Navbar = () => {
         >
           Videos
         </NavLink>
-        <NavLink
+        {user.email ? <button className="logout-button" onClick={handleLogout}>Logout</button> : <NavLink
           className={({ isActive }) => (isActive ? "active-link" : "link")}
           to='/login'
         >
           Login
-        </NavLink>
+        </NavLink>}
       </div>
     </nav>
   );
